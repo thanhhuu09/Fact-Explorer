cbaeea4addb094a7ca6d81c6b0d040daf0080a6d
Tommy Thach <thachtommy5@gmail.com>
2023-05-04 11:09:38 +0700

    add .env

--- src/supabase.js
+++ src/supabase.js
@@ -1,11 +1,7 @@
-// import * as dotenv from "dotenv";
-
 import { createClient } from "@supabase/supabase-js";
-// dotenv.config();
 
-const supabaseUrl = "https://ppyxnenbiyiqweoqpext.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBweXhuZW5iaXlpcXdlb3FwZXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNjc0NTcsImV4cCI6MTk5Mzk0MzQ1N30.QecaPCwDLSHpBfc--jrwCj0UtIJdKGJQRLnL9amAhSY";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
